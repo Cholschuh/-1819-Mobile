9a43b3154b3effc78572c71bc918df76cd76bff6
“Chris <holschcj@ucmail.uc.edu>
2020-01-25 15:15:05 -0500

    Install KontaktSDK 3.0.1, Add Import to view controller and AppDelegate, add API to AppDelegate

--- -1819 Mobile/ViewController.swift
+++ -1819 Mobile/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import KontaktSDK
 
 class ViewController: UIViewController {
 
---
+++ Pods/Target Support Files/Pods--1819 Mobile/Pods--1819 Mobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods__1819_Mobile : NSObject
+@end
+@implementation PodsDummy_Pods__1819_Mobile
+@end
