3e00c41b4d84d98bbccce1fe96c473874524da6e
“Chris <holschcj@ucmail.uc.edu>
2020-01-25 14:38:44 -0500

    Initial Commit

---
+++ -1819 Mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  -1819 Mobile
+//
+//  Created by Chris Holschuh on 1/25/20.
+//  Copyright © 2020 Chris Holschuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
