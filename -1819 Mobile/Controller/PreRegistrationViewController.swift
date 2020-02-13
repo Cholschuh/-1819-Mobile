b5e3655de745aa18e645945a17e5709adb181f75
“Chris <holschcj@ucmail.uc.edu>
2020-02-13 02:24:12 -0500

    Add some email configs

--- -1819 Mobile/Controller/PreRegistrationViewController.swift
+++ -1819 Mobile/Controller/PreRegistrationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class PreRegistrationViewController: UIViewController {
 
@@ -14,6 +15,14 @@ class PreRegistrationViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var email: UITextField!
+    
+    @IBAction func preregisterBtn(_ sender: UIButton) {
+        let usersName = name.text
+        let usersEmail = email.text
     }
     
 
@@ -26,5 +35,40 @@ class PreRegistrationViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+//    func showMailComposer(name: String, email: String? ){
+//        guard MFMailComposeViewController.canSendMail() else {
+//            print("Email is not supported on this device")
+//            return
+//        }
+//        let composer =  MFMailComposeViewController()
+//        composer.mailComposeDelegate = self
+//        composer.setToRecipients(["Chris.csrstaff@gmail.com"])
+//        composer.setSubject("\(name.text)-Pregistation request")
+//        composer.setMessageBody("It works! See See. My prefered email is  \(email.text)", isHTML: false)
+//        present(composer, animated: true)
+//    }
+    
+}
+extension PreRegistrationViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            controller.dismiss(animated: true, completion: nil)
+        }
+        
+        switch result {
+        case .cancelled:
+            print("Cancelled")
+        case .failed:
+            print("Failed to send")
+        case .saved:
+            print("Saved")
+            case .sent:
+            print("Sent!")
+        default:
+            print("Not accounted for")
+        }
+        
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
