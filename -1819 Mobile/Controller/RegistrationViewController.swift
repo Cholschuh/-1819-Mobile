c12b9b3c3c3fda8c316b8fef833db6a451ff1223
“Chris <holschcj@ucmail.uc.edu>
2020-02-15 17:27:55 -0500

    Add Backend validation for Pre-registration email

--- -1819 Mobile/Controller/RegistrationViewController.swift
+++ -1819 Mobile/Controller/RegistrationViewController.swift
@@ -12,33 +12,58 @@ import MessageUI
 class RegistrationViewController: UIViewController{
     @IBOutlet weak var nameTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
-    
+    var usersFullName: String?
+    var usersPreferredEmail: String?
     var messageSent: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ///calls extension functions to dismiss keyboard when textfield looses focus
-         self.setupHideKeyboardOnTap()
+        self.setupHideKeyboardOnTap()
     }
-
+    
     @IBAction func preRegisterBtn(_ sender: UIButton) {
-        let usersFullName = nameTxt.text ?? "No name given"
-        let usersEmail = emailTxt.text ?? "No email given"
-        showMailComposer(usersFN: usersFullName, usersPrefEmail: usersEmail)
-    }
-    func showMailComposer(usersFN: String, usersPrefEmail: String){
-            guard MFMailComposeViewController.canSendMail() else {
-                print("Email is not supported on this device")
+        usersFullName = nameTxt.text
+        usersPreferredEmail = emailTxt.text
+        
+        if !usersFullName!.isEmpty && !usersPreferredEmail!.isEmpty{
+            if validationHelper.isEmailVaild(emailAddress: usersPreferredEmail!){
+                showMailComposer(usersFN: usersFullName!, usersPrefEmail: usersPreferredEmail!)
+            }else{
+                print("email invaild")
                 return
             }
-            let composer =  MFMailComposeViewController()
-            composer.mailComposeDelegate = self
-            composer.setToRecipients(["Chris.csrstaff@gmail.com"])
-            composer.setSubject("\(usersFN)'s Pre-registation request")
-            composer.setMessageBody("It works! See See. My prefered email is \(usersPrefEmail)", isHTML: false)
-            present(composer, animated: true)
         }
-
+        else if !usersFullName!.isEmpty && usersPreferredEmail!.isEmpty{
+            showMailComposer(usersFN: usersFullName!, usersPrefEmail: "NA")
+        }
+        else{
+            print("username is required")
+            return
+        }
+    }
+    
+    
+    func showMailComposer(usersFN: String, usersPrefEmail: String){
+        guard MFMailComposeViewController.canSendMail() else {
+            print("Email is not supported on this device")
+            return
+        }
+        var messageBody: String = "Hi my name is \(usersFN). I am reaching out to you today as I would like to pre-registered for the check-in process at 1819."
+        if usersPrefEmail == "NA"{
+            messageBody.append("Please use the email address I am sending from as my prefered email for this registration.")
+        }else {
+            messageBody.append("Please use \(usersPrefEmail) as my prefered email for this registration.")
+        }
+        
+        let composer =  MFMailComposeViewController()
+        composer.mailComposeDelegate = self
+        composer.setToRecipients(["Chris.csrstaff@gmail.com"])
+        composer.setSubject("\(usersFN)'s Pre-registation request")
+        composer.setMessageBody(messageBody, isHTML: false)
+        present(composer, animated: true)
+    }
+    
 }
 extension RegistrationViewController: MFMailComposeViewControllerDelegate, UITextFieldDelegate{
     
@@ -47,18 +72,18 @@ extension RegistrationViewController: MFMailComposeViewControllerDelegate, UITex
             print(error.localizedDescription)
             controller.dismiss(animated: true)
         }
-            switch result {
-            case .cancelled:
-                print("Cancelled")
-            case .failed:
-                print("Failed to send")
-            case .saved, .sent:
-                print("Sent")
-            default:
-                print("Not accounted for")
-            }
-            controller.dismiss(animated: true, completion: nil)
+        switch result {
+        case .cancelled:
+            print("Cancelled")
+        case .failed:
+            print("Failed to send")
+        case .saved, .sent:
+            print("Sent")
+        default:
+            print("Not accounted for")
         }
+        controller.dismiss(animated: true, completion: nil)
+    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         nameTxt.resignFirstResponder()
         emailTxt.resignFirstResponder()
@@ -70,7 +95,7 @@ extension RegistrationViewController: MFMailComposeViewControllerDelegate, UITex
         self.view.addGestureRecognizer(self.endEditingRecognizer())
         self.navigationController?.navigationBar.addGestureRecognizer(self.endEditingRecognizer())
     }
-
+    
     /// Dismisses the keyboard from self.view
     private func endEditingRecognizer() -> UIGestureRecognizer {
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing(_:)))
@@ -78,5 +103,5 @@ extension RegistrationViewController: MFMailComposeViewControllerDelegate, UITex
         return tap
     }
     
-    }
+}
 
