e5279a37f1ebf209e9dde31e8d8fe8872ac4613b
“Chris <holschcj@ucmail.uc.edu>
2020-02-27 00:57:48 -0500

    added UIGesters needs some more tunning

--- -1819 Mobile/Controller/FloorMapViewController.swift
+++ -1819 Mobile/Controller/FloorMapViewController.swift
@@ -11,13 +11,9 @@ import UIKit
 
 class FloorMapViewController: UIViewController {
     var floorObj: FloorsMO?
+    var imageZoomed: Bool = false
     var selectedFloorNameImagePath: String = ""
     var selectedFloorName: String = ""
-    
-    @IBAction func rotateImage(_ sender: Any) {
-    }
-    @IBAction func tapImage(_ sender: Any) {
-    }
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
@@ -27,20 +23,72 @@ class FloorMapViewController: UIViewController {
         }else{
             print("Not able to load room object")
         }
-        
-        
     }
-override func viewWillLayoutSubviews() {
+    
+    
+    override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
     }
     
-    @IBAction func scaleImage(_ sender: Any) {
-        imageView.transform = CGAffineTransform(scaleX: (sender as AnyObject).scale, y: (sender as AnyObject).scale)
+    @IBAction func panImage(_ recognizer: UIPanGestureRecognizer) {
+        
+        guard let recognizerView = recognizer.view else{return}
+        
+        let translation =  recognizer.translation(in: view)
+        recognizerView.center.x += translation.x
+        recognizerView.center.y += translation.y
+        recognizer.setTranslation(.zero, in: view)
+        
+        
+    }
+    @IBAction func rotateImage(_ recognizer: UIRotationGestureRecognizer) {
+        
+        if let recognizerView = recognizer.view {
+            recognizerView.transform = imageView.transform.rotated(by: recognizer.rotation)
+            recognizer.rotation = 0
         }
+    }
+    @IBAction func tapImage(_ sender: Any) {
+        if (!imageZoomed) {
+            scaleImageUp()
+            imageZoomed = true
+        } else {
+            normalizeImage()
+            imageZoomed = false
+        }
+    }
+    @IBAction func scaleImage(_ recognizer: UIPinchGestureRecognizer) {
+        
+        guard let recognizerView = recognizer.view else{return}
+        
+        recognizerView.transform = recognizerView.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+        recognizer.scale = 1
+        //        imageView.transform = CGAffineTransform(scaleX: (sender as AnyObject).scale, y: (sender as AnyObject).scale)
+        
+    }
     
     func configureView(floorObj: FloorsMO){
         guard let selectedFloorNameImagePath = floorObj.mapImage else{return}
         guard let selectedFloorName = floorObj.name else{return}
         imageView.image = UIImage(named: selectedFloorNameImagePath)
+        imageView.isAccessibilityElement = true
+        imageView.accessibilityLabel = "A map image of \(selectedFloorName) Rooms"
+    }
+    
+    func scaleImageUp() {
+        imageView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        imageZoomed = true
+    }
+    
+    func normalizeImage() {
+        imageView.transform = CGAffineTransform.identity
+        imageZoomed = false
+    }
+    
+}
+extension FloorMapViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
